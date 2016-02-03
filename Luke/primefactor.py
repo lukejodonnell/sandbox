1afc21d722d042a62a8cb3b8a99699c8aa249802
Luke O'Donnell <lukejodonnell@gmail.com>
2016-02-03 16:08:34 -0500

    cleaned up the primefactor calculator, added ability ot calculate negative numbers, 0, and 1

--- Luke/primefactor.py
+++ Luke/primefactor.py
@@ -1,26 +1,37 @@
 running = True
 
-#The following three lines import a list of 1000 prime numbers from the file PrimeNumbersList.txt and store it in the list primes[]
+#The following four lines import a list of 1000 prime numbers from the file PrimeNumbersList.txt and store it in the list primes[]
 f = open("./PrimeNumbersList.txt")
 primes = f.readlines()
 primes = list(map(int, primes))
+f.close()
 
 def primefac(number):
 	index = 0
-	while number >= primes[index]:
-		dontMoveOn = True
-		while dontMoveOn:
-			if number/primes[index] == number//primes[index]:
-				number = number / primes[index]
-				print(primes[index])
-			else:
-				dontMoveOn = False
-				index = index + 1
-	if number != 1:
-		print(number)
+	if number < 0:
+		number = -number
+		print(-1)
+	if number == 1:
+		print("1 (kinda, 1 is not a prime number)")
+	if number == 0:
+		print("0 is just weird man, it has every factor")
+	if number != 1 and number != 0:
+		while number >= primes[index]:
+			dontMoveOn = True
+			while dontMoveOn:
+				if number/primes[index] == number//primes[index]:
+					number = number / primes[index]
+					print(primes[index])
+				else:
+					dontMoveOn = False
+					index = index + 1
+		if number != 1:
+			print("sorry, couldn't finish the problem, here is the remainder")
+			print(number)
 		
 while running:
-	testnum = int(input("what do you want the prime factorization of?"))
+	testnum = int(input("what do you want the prime factorization of? "))
 	primefac(testnum)
 	if input("Do you want to factorize another number? y/n ") != "y":
 		running = False
+		print("okay goodbye!")
